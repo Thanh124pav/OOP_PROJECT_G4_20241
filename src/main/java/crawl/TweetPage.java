a084535de2e32b0d5d171c335b5df1a1d162553e
Thanh Pham Van <phamvanthanh.csp@gmail.com>
2024-12-11 08:39:54 +0700

    fix attributes of UserPage and TweetPage

--- src/main/java/crawl/TweetPage.java
+++ src/main/java/crawl/TweetPage.java
@@ -1,6 +1,7 @@
 package crawl;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import model.Tweet;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,72 +17,109 @@ public class TweetPage extends Page{
     protected static String replyCard = primaryColumn +  "button[data-testid='reply'] span.css-1jxf684.r-1ttztb7.r-qvutc0.r-poiln3.r-n6v787.r-1cwl3u0.r-1k6nrdp.r-n7gxbd";
     protected static String timeCard = primaryColumn + "a.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3.r-xoduu5.r-1q142lx.r-1w6e6rj.r-9aw3ui.r-3s2u2q.r-1loqt21 time";
 
-    //Basic attributes
-    private String tweetUrl;
-    private String tweetId;
-    private String author;
-
-    //Detail attributes
-    private String content;
-    private Set<String> hashtags = new HashSet<>();
-    private String repliesCount;
-    private String retweetsCount;
-    private String likesCount;
-    private String createdAt;
+    private Tweet tweet;
+//    //Basic attributes
+//    private String tweetUrl;
+//    private String tweetId;
+//    private String author;
+//
+//    //Detail attributes
+//    private String content;
+//    private Set<String> hashtags = new HashSet<>();
+//    private String repliesCount;
+//    private String retweetsCount;
+//    private String likesCount;
+//    private String createdAt;
 
     //Advance attributes
-    private List<String> retweeters = new ArrayList<>();
+    //private List<String> retweeters = new ArrayList<>();
 
     public TweetPage() {}
 
-    public String getTweetUrl() {
-        return tweetUrl;
-    }
-
     public TweetPage(String link) {
-        this.tweetUrl = link;
+        //this.tweetUrl = link;
+        tweet = new Tweet(link);
         extractBasicInfo(link);
     }
     private void extractBasicInfo(String link){
         String[] partsOfLink = link.split("/");
         if(partsOfLink.length >=4){
-            this.author = partsOfLink[3];
+            //this.author = partsOfLink[3];
+            tweet.setAuthor(partsOfLink[3]);
         }
         if(partsOfLink.length >=6){
-            this.tweetId = partsOfLink[5];
+            //this.tweetId = partsOfLink[5];
+            tweet.setTweetId(partsOfLink[5]);
         }
 
     }
-    public void extractDetails(WebDriver driver) throws InterruptedException, IOException {
-        driver.get(tweetUrl);
-        Thread.sleep(5000);
-
-        content = driver.findElement(By.cssSelector(contentCard)).getText();
-        Set<WebElement> hashtagCards = new HashSet<>(driver.findElements(By.cssSelector(TweetPage.hashtagCard)));
-        if(!hashtagCards.isEmpty()){
-            for (WebElement hashtagCard : hashtagCards) {
-                String hashtag = hashtagCard.getText();
-                if(!hashtag.isEmpty()){
-                    if(hashtag.charAt(0) == '#'){
-                        hashtags.add(hashtag);
+    public void extractContent(WebDriver driver){
+        String content ;
+        try {
+            content = driver.findElement(By.cssSelector(contentCard)).getText();
+        }catch (NoSuchElementException e){
+            content = "";
+        }
+        tweet.setContent(content);
+    }
+    public void extractHashtag(WebDriver driver){
+        Set<String> hashtags = new HashSet<>();
+        try {
+            Set<WebElement> hashtagCards = new HashSet<>(driver.findElements(By.cssSelector(TweetPage.hashtagCard)));
+            if(!hashtagCards.isEmpty()){
+                for (WebElement hashtagCard : hashtagCards) {
+                    String hashtag = hashtagCard.getText();
+                    if(!hashtag.isEmpty()){
+                        if(hashtag.charAt(0) == '#'){
+                            hashtags.add(hashtag);
+                        }
                     }
                 }
             }
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
         }
+        tweet.setHashtags(new ArrayList<>(hashtags));
+    }
+    public void extractDetails(WebDriver driver) throws InterruptedException, IOException {
+        //driver.get(tweetUrl);
+        driver.get(tweet.getTweetUrl());
+        Thread.sleep(5000);
 
-        repliesCount = driver.findElement(By.cssSelector(replyCard)).getText() ;
-        retweetsCount =  driver.findElement(By.cssSelector(retweetCard)).getText() ;
-        likesCount = driver.findElement(By.cssSelector(likesCard)).getText() ;
-        createdAt = driver.findElement(By.cssSelector(timeCard)).getAttribute("datetime");
+        //content = driver.findElement(By.cssSelector(contentCard)).getText();
+        extractContent(driver);
+//        Set<WebElement> hashtagCards = new HashSet<>(driver.findElements(By.cssSelector(TweetPage.hashtagCard)));
+//        if(!hashtagCards.isEmpty()){
+//            for (WebElement hashtagCard : hashtagCards) {
+//                String hashtag = hashtagCard.getText();
+//                if(!hashtag.isEmpty()){
+//                    if(hashtag.charAt(0) == '#'){
+//                        hashtags.add(hashtag);
+//                    }
+//                }
+//            }
+//        }
+        extractHashtag(driver);
+
+//        repliesCount = driver.findElement(By.cssSelector(replyCard)).getText() ;
+//        retweetsCount =  driver.findElement(By.cssSelector(retweetCard)).getText() ;
+//        likesCount = driver.findElement(By.cssSelector(likesCard)).getText() ;
+//        createdAt = driver.findElement(By.cssSelector(timeCard)).getAttribute("datetime");
+        tweet.setRepliesCount(driver.findElement(By.cssSelector(replyCard)).getText());
+        tweet.setRetweetsCount(driver.findElement(By.cssSelector(retweetCard)).getText());
+        tweet.setLikesCount(driver.findElement(By.cssSelector(likesCard)).getText());
+        tweet.setCreatedAt(driver.findElement(By.cssSelector(timeCard)).getAttribute("datetime"));
 
     }
 
     public void extractRetweeters(WebDriver driver, int limit) throws InterruptedException, IOException {
-        driver.get(tweetUrl + "/retweets");
+        //driver.get(tweetUrl + "/retweets");
+        driver.get(tweet.getTweetUrl() + "/retweets");
         Thread.sleep(5000);
 
         Set<String> elements = getElementsByScroll(driver, limit, Page.userCard);
-        retweeters.addAll(elements);
+        //retweeters.addAll(elements);
+        tweet.setRetweeters(new ArrayList<>(elements));
     }
 
     public void extractAllInfo(WebDriver driver, int limitRetweets) throws InterruptedException, IOException {
@@ -101,53 +139,59 @@ public class TweetPage extends Page{
         return ids;
     };
 
-    public String getTweetId() {
-        return tweetId;
+//    public String getTweetId() {
+//        return tweetId;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public Set<String> getHashtags() {
+//        return hashtags;
+//    }
+//
+//    public String getRepliesCount() {
+//        return repliesCount;
+//    }
+//
+//    public String getRetweetsCount() {
+//        return retweetsCount;
+//    }
+//
+//    public String getLikesCount() {
+//        return likesCount;
+//    }
+//
+//    public String getCreatedAt() {
+//        return createdAt;
+//    }
+
+    public Tweet getTweet() {
+        return tweet;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public Set<String> getHashtags() {
-        return hashtags;
-    }
-
-    public String getRepliesCount() {
-        return repliesCount;
-    }
-
-    public String getRetweetsCount() {
-        return retweetsCount;
-    }
-
-    public String getLikesCount() {
-        return likesCount;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public List<String> getRetweeters() {
-        return retweeters;
-    }
+//    public List<String> getRetweeters() {
+//        return retweeters;
+//    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TweetPage tweetPage = (TweetPage) o;
-        return Objects.equals(tweetId, tweetPage.tweetId);  // So sánh dựa trên thuộc tính id
+        //return Objects.equals(tweetId, tweetPage.tweetId);  // So sánh dựa trên thuộc tính id
+        return Objects.equals(tweet.getTweetUrl(), tweetPage.tweet.getTweetUrl());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(tweetId);
+        //return Objects.hash(tweetId);
+        return Objects.hash(tweet.getTweetId());
     }
 
 
@@ -173,16 +217,22 @@ public class TweetPage extends Page{
                 tweetLinksCrawl.add(link);
                 i+=1;
                 System.out.println("\t" + link);
-                TweetPage tweet = new TweetPage(link);
-                UserPage user = new UserPage(tweet.getAuthor());
-                tweet.extractAllInfo(driver, 30);
-                tweetsCrawl.add(tweet);
-                if(!userIdCrawl.contains(tweet.getAuthor())){
-                    userIdCrawl.add(tweet.getAuthor());
-                    user.extractAllInfo(driver, 5, 30);
-                    usersCrawl.add(user);
+                TweetPage tweetPage = new TweetPage(link);
+                //UserPage user = new UserPage(tweet.getAuthor());
+                UserPage userPage = new UserPage(tweetPage.tweet.getAuthor());
+                tweetPage.extractAllInfo(driver, 30);
+                tweetsCrawl.add(tweetPage);
+//                if(!userIdCrawl.contains(tweet.getAuthor())){
+//                    userIdCrawl.add(tweet.getAuthor());
+//                    user.extractAllInfo(driver, 5, 30);
+//                    usersCrawl.add(user);
+//                }
+                if(!userIdCrawl.contains(tweetPage.tweet.getAuthor())){
+                    userIdCrawl.add(tweetPage.tweet.getAuthor());
+                    userPage.extractAllInfo(driver, 5, 30);
+                    usersCrawl.add(userPage);
                 }
-                for(String userId: tweet.getRetweeters()){
+                for(String userId: tweetPage.tweet.getRetweeters()){
                     if(!userIdCrawl.contains(userId)){
                         userIdCrawl.add(userId);
                         UserPage retweeter = new UserPage(userId);
--- src/main/java/crawl/UserPage.java
+++ src/main/java/crawl/UserPage.java
@@ -1,16 +1,14 @@
 package crawl;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import model.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class UserPage extends Page{
     protected static String userNameCard = primaryColumn + "div[data-testid='UserName'] span.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3";//"span.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3";
@@ -18,119 +16,136 @@ public class UserPage extends Page{
     protected static String imageCard = primaryColumn + "a.css-175oi2r.r-1pi2tsx.r-13qz1uu.r-o7ynqc.r-6416eg.r-1ny4l3l.r-1loqt21";
     protected static String followCard = primaryColumn + "span.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3.r-1b43r93.r-1cwl3u0.r-b88u0q";
 
-    private String profileUrl;
-    private String userId;
+//    private String profileUrl;
+//    private String userId;
+//
+//    private String userName;
+//    private String bio;
+//
+//    private String profileImageUrl;
+//
+//    public String getProfileUrl() {
+//        return profileUrl;
+//    }
+//
+//    public String getUserId() {
+//        return userId;
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public String getBio() {
+//        return bio;
+//    }
+//
+//    public String getProfileImageUrl() {
+//        return profileImageUrl;
+//    }
+//
+//    public String getFollowersCount() {
+//        return followersCount;
+//    }
+//
+//    public String getFollowingCount() {
+//        return followingCount;
+//    }
+//
+//    public int getTweetCount() {
+//        return tweetCount;
+//    }
+//
+//    public Set<String> getTweets() {
+//        return tweets;
+//    }
+//
+//    public Set<String> getFollowing() {
+//        return following;
+//    }
 
-    private String userName;
-    private String bio;
+//    private String followersCount;
+//    private String followingCount;
+//
+//    private int tweetCount;
+//    private Set<String >tweets; // (tweetId)
+//    private Set<String> following; // (<1000) (userId)
 
-    private String profileImageUrl;
+    private User user;
 
-    public String getProfileUrl() {
-        return profileUrl;
-    }
+    public UserPage(){}
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public String getProfileImageUrl() {
-        return profileImageUrl;
-    }
-
-    public String getFollowersCount() {
-        return followersCount;
-    }
-
-    public String getFollowingCount() {
-        return followingCount;
-    }
-
-    public int getTweetCount() {
-        return tweetCount;
-    }
-
-    public Set<String> getTweets() {
-        return tweets;
-    }
-
-    public Set<String> getFollowing() {
-        return following;
-    }
-
-    private String followersCount;
-    private String followingCount;
-
-    private int tweetCount;
-    private Set<String >tweets; // (tweetId)
-    private Set<String> following; // (<1000) (userId)
-
-    public UserPage(){};
     public UserPage(String userId){
-        this.userId = userId;
-        this.profileUrl = "https://x.com/" + userId;
+//        this.userId = userId;
+//        this.profileUrl = "https://x.com/" + userId;
+        this.user = new User(userId);
+        user.setProfileUrl("https://x.com/" + userId);
     }
 
     public void extractDetails(WebDriver driver) throws InterruptedException, IOException {
-        driver.get(profileUrl);
+        //driver.get(profileUrl);
+        driver.get(this.user.getProfileUrl());
         Thread.sleep(3000);
-        userName = driver.findElement(By.cssSelector(userNameCard)).getText();
+        //userName = driver.findElement(By.cssSelector(userNameCard)).getText();
+        this.user.setUsername(driver.findElement(By.cssSelector(userNameCard)).getText());
         try {
             WebElement bioElement = driver.findElement(By.cssSelector(bioCard));
-            bio =  bioElement.getText();
+            //bio =  bioElement.getText();
+            user.setBio(bioElement.getText());
         } catch (NoSuchElementException e) {
-            bio = "";
+            //bio = "";
+            user.setBio("");
         }
-        profileImageUrl = driver.findElement(By.cssSelector(imageCard)).getText();
+        //profileImageUrl = driver.findElement(By.cssSelector(imageCard)).getText();
         List<WebElement> follow = driver.findElements(By.cssSelector(followCard));
-        followingCount = follow.getFirst().getText();
-        followersCount = follow.getLast().getText();
+        //followingCount = follow.getFirst().getText();
+        user.setFollowingCount(follow.getFirst().getText());
+        //followersCount = follow.getLast().getText();
+        user.setFollowersCount(follow.getLast().getText());
     }
 
     public void extractTweets(WebDriver driver, int limit) throws InterruptedException, IOException {
-        driver.get(profileUrl);
+        //driver.get(profileUrl);
+        driver.get(this.user.getProfileUrl());
         Thread.sleep(4000);
-        tweets = getElementsByScroll(driver, limit, Page.tweetCard);
-        tweetCount = tweets.size();
+        //tweets = getElementsByScroll(driver, limit, Page.tweetCard);
+        Set<String> tweets = getElementsByScroll(driver, limit, Page.tweetCard);
+        user.setTweets(new ArrayList<>(tweets));
+        //tweetCount = tweets.size();
+        user.setTweetCount(tweets.size());
     }
 
     public boolean checkFollowersCount(){
         int count = -1;
         try {
-            count = Integer.parseInt(followersCount);
-            if (count > 0) { // có thể chuyển sang số nguyên được -->
-                return false;
-            }
-        } catch (NumberFormatException e) { // số lượng followers lớn hơn 1000
-            String[] parts = followersCount.split(" ");
-            if(parts.length < 2){
-                return false;
-            }
-            if(!parts[1].equals("N")){
-                return true;
-            }
-            String[] subParts = parts[0].split(",");
-            int firstDigit = Integer.parseInt(subParts[0]);
-            if(firstDigit >= 5){
-                return true;
-            }
+            //count = Integer.parseInt(followersCount);
+            count = Integer.parseInt(user.getFollowersCount());
+            return (count > 300);
+        } catch (NumberFormatException e) {
+            return true;// số lượng followers lớn hơn 1000
+//            String[] parts = followersCount.split(" ");
+//            if(parts.length < 2){
+//                return false;
+//            }
+//            if(!parts[1].equals("N")){
+//                return true;
+//            }
+//            String[] subParts = parts[0].split(",");
+//            int firstDigit = Integer.parseInt(subParts[0]);
+//            if(firstDigit >= 5){
+//                return true;
+//            }
         }
-        return false;
     }
 
     public void extractFollowing(WebDriver driver, int limit) throws InterruptedException, IOException {
-        driver.get(profileUrl + "/following");
+        //driver.get(profileUrl + "/following");
+        driver.get(this.user.getProfileUrl());
         Thread.sleep(4000);
 
-        following = getElementsByScroll(driver, limit, Page.userCard);
+        //following = getElementsByScroll(driver, limit, Page.userCard);
+        Set<String> following = getElementsByScroll(driver, limit, Page.userCard);
+        user.setFollowing(new ArrayList<>(following));
     }
 
     public void extractAllInfo(WebDriver driver, int limitTweets, int limitFollowing) throws InterruptedException, IOException {
@@ -155,12 +170,17 @@ public class UserPage extends Page{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserPage userPage = (UserPage) o;
-        return Objects.equals(userId, userPage.userId);  // So sánh dựa trên thuộc tính id
+        //return Objects.equals(userId, userPage.userId);  // So sánh dựa trên thuộc tính id
+        return Objects.equals(user.getUserId(), userPage.user.getUserId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId);
+        //return Objects.hash(userId);
+        return Objects.hash(user.getUserId());
     }
 
+    public User getUser() {
+        return user;
+    }
 }
